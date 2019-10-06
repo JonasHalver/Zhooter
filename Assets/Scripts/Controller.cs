a554b14d9b7f56f9fd946c03a4f13cbe25ba232d
JonasHalver <44400498+JonasHalver@users.noreply.github.com>
2019-10-06 16:36:22 +0200

    Enemy added

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public SpriteRenderer sr;
+    GameObject player;
+    Vector2 dirToTarget;
+
+    public GameObject target;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target == null)        
+            target = player;
+
+        dirToTarget = target.GetComponent<Rigidbody2D>().position - rb.position;
+    }
+
+    void FixedUpdate()
+    {
+        if (target != null)
+        {
+            float angle = Mathf.Atan2(dirToTarget.y, dirToTarget.x) * Mathf.Rad2Deg - 90f;
+            rb.rotation = angle;
+        }
+    }
+
+    public void Hit(Color hitColor)
+    {
+        StartCoroutine(HitColor(hitColor));
+    }
+
+    IEnumerator HitColor(Color hitColor)
+    {
+        Color c = sr.color;
+
+        sr.color = hitColor;
+        yield return new WaitForSeconds(0.01f);
+
+        sr.color = new Color(c.r * 0.5f, c.g * 0.5f, c.b * 0.5f);
+    }
+}
